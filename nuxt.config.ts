c02367c2c94cc1324cecd8f996fef211f7de2934
Kenneth Moren <kenneth.moren@live.no>
2026-05-20 14:32:14 +0200

    icon
    fikk inn mitt eget icon når nettleseren lastes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,26 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss'],
+
+  app: {
+    head: {
+      title: 'CV-mal',
+      htmlAttrs: {
+        lang: 'no' // Setter språket på nettsiden til norsk
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: 'cv-mal' },
+      ],
+      link: [
+        // Hvis du har et favicon i public/mappen:
+        { rel: 'icon', type: 'image/x-icon', href: '/icon-svg.svg' }
+      ]
+    }
+  }
+
+
+  
 })
